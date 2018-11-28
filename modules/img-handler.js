2aa087998c906a2a1bd62a39dca46fa041cb6bd7
zerihund <33481451+zerihund@users.noreply.github.com>
2018-11-28 10:28:49 +0200

    server-handler and img -handler edit to test on the server

--- modules/img-handler.js
+++ modules/img-handler.js
@@ -1,10 +1,20 @@
 //for handle image processing on server
 //change picture size
-
+'use strict';
 const sharp = require('sharp');
-const resize = (filepath, width, newfilepath) =>{
-  sharp(filepath)
-      .resize(width)
-      .toFile(newfilepath);
 
+const doResize = (filePath, width, newPath, next) => {
+  sharp(filePath)
+  .resize(width)
+  .toFile(newFilePath)
+  .then(() => {
+    console.log('Resize OK');
+    next();
+  }).catch(err => {
+    console.log(err)
+  });
+};
+
+modules.exports = {
+  doResize: doResize,
 };
