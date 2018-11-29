2c6a8c5566c06290cdf88996413a3e078575eedc
ASD <ngocson.bui0312@gmail.com>
2018-11-29 15:28:45 +0200

    continuous bug fixing 0.9

--- server-handler.js
+++ server-handler.js
@@ -81,13 +81,14 @@ app.get('/grabstory', (req, res)=>{
     storybranch.unshift(results);
     console.log('--------');
     console.log(storybranch);
-
+    let i =0;
     //get parent story and append it to begin of story branch array
     do{
+      i++;
       db.getParentStory(connection,storybranch[0].story_id)
       .then(result =>
           storybranch.unshift(result))
-    }while(storybranch[0].parent !== 0);
+    }while(storybranch[0].parent !== 0 || i==1);
 
     console.log(storybranch);
     //get comments for each story
