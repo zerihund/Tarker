2b66da895350415a9fb07f7619b7a9f1e7ceb664
ASD <ngocson.bui0312@gmail.com>
2018-11-29 15:38:47 +0200

    continuous bug fixing 0.11

--- modules/data-handler.js
+++ modules/data-handler.js
@@ -83,6 +83,7 @@ const getInitStory = (connection)=>{
        WHERE c.story_Id != Isparent.story_Id`,
       (err, results)=>{
         console.log(results);
+        console.log(results[0]);
         resolve(results);
       }
     )
