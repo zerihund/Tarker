89bd92f8be39c9627eddbd9de0f2b2a384833a8f
zerihund <33481451+zerihund@users.noreply.github.com>
2018-12-03 00:44:25 +0200

    calculation error has been fixed

--- modules/math.js
+++ modules/math.js
@@ -7,22 +7,23 @@ const random = (max)=>{
 //let n be the sample size, and pos be the amount of positive(liked stories in our case score =-1)
 // calculates the binomial proportion confidence interval
 // lower bound of a bernoulli parameter....we take the lower bound to decide which is story is most liked
-const lowerBound = (pos, n) => {
+const wilson = (pos, n) => {
   if (n === 0) {
     return 0
   }
   else {
-    //z score for 95% confidence. we can also calculate z using z = Statistics2.pnormaldist(1-(1-confidence)/2) the result is between 1.8 and 1.98
+    //z score for 95% confidence. we can also calculate z using z = Statistics2.pNormalDist(1-(1-confidence)/2) the result is between 1.64 and 2.3
+    //for simplicity we use 1.96 most known approximation 
     const z = 1.96;
-    const pbound = 1.0 * upvotes / n
+    const pBound = pos/ n;
 
-    return (pbound + z * z / (2 * n) - z *
-        Math.sqrt((pbound * (1 - pbound) + z * z / (4 * n)) / n)) /
+    return (pBound + z * z / (2 * n) - z *
+        Math.sqrt((pBound * (1 - pBound) + z * z / (4 * n)) / n)) /
         (1 + z * z / n)
   }
 }
 
 module.exports = {
   random:random,
-  lowerBound:lowerBound,
+  wilson:wilson,
 };
