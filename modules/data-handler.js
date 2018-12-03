82cbb6fddfa6962de7e06535204e33dec4bcbb36
ASD <ngocson.bui0312@gmail.com>
2018-12-03 16:06:20 +0200

    i hope all the query work now, at least for the grabbing of stories

--- modules/data-handler.js
+++ modules/data-handler.js
@@ -98,7 +98,7 @@ const getParentStory = (connection, id) =>{
     connection.query(
       `SELECT c.story_Id, c.parent_story, c.content
       FROM story c, story d
-      WHERE c.story_id = d.parent_story AND d.story_Id = ${id}`,
+      WHERE c.story_id = d.parent_story AND d.story_Id = '${id}'`,
       (err, results)=>{
         console.log('------abc------');
         resolve(results);
