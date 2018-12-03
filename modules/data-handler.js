c529d20ecf112d949920a3a85b5f4b69a33bc553
ASD <ngocson.bui0312@gmail.com>
2018-12-03 13:31:13 +0200

    init storm 0.0.13 continue upload test. hopefully it works now

--- modules/data-handler.js
+++ modules/data-handler.js
@@ -168,7 +168,7 @@ const upload = (connection, data, res)=>{
   const story = data[3];
   console.log(author +' '+ parent +' '+title + ' '+ story);
   const storyid = math.idGenerate();
-  console.log(`INSERT INTO story (story_Id, title,content,parent_story,media,story_Flag) VALUES (${storyid},'${title}','${story}',${parent},'something here', null)`);
+  console.log(`INSERT INTO story (story_Id, title,content,parent_story,media,story_Flag) VALUES ('${storyid}','${title}','${story}',${parent},'something here', null)`);
   console.log(storyid);
   connection.query(
       //'INSERT INTO story (story_Id,content,parent_story,media,story_Flag) VALUES (12345,"heloolwo oid",14,"ddi",null)',
