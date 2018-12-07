5fc2664e8567bfeb4ab0830f321b0a98ba9b8093
zerihund <33481451+zerihund@users.noreply.github.com>
2018-12-07 10:13:24 +0200

    rechecking the select again

--- modules/data-handler.js
+++ modules/data-handler.js
@@ -156,11 +156,8 @@ const getOpinion = (connection, id)=>{
 };
  const putOpinion= (connection, data) =>{
    connection.query(
-      ` UPDATE Views
-   SET Views.like_story =${data[2]}
-   WHERE Views.user_Id =${data[0]} and Views.story_Id=${data[1]}`,
-       /*`INSERT INTO Views (Views.user_Id,Views.story_Id,Views.like_story,Views.view_count)
-        VALUES(${data[0]},${data[2]},${data[1]}, null)`,*/
+       `INSERT INTO Views (Views.user_Id,Views.story_Id,Views.like_story,Views.view_count)
+        VALUES(${data[0]},${data[2]},${data[1]}, null)`,
        (err, results)=>{
          console.log(results);
          return(results);
