0dc9377eeaac6cd2f2c794fe406209c1ce1df608
ASD <ngocson.bui0312@gmail.com>
2018-12-10 18:21:56 +0200

    fixing some bug on moderate.js

--- public/res/js/moderate.js
+++ public/res/js/moderate.js
@@ -2,7 +2,7 @@ document.querySelector('.login-form').addEventListener('submit', evt => {
   evt.preventDefault();
   const name = evt.target[1];
   const password  = evt.target[3];
-  fetch('/moderatorlog', {
+  fetch('/node/moderatorlog', {
     method: 'POST',
     headers: new Headers({
       'Content-Type': 'application/x-www-form-urlencoded',
